967e74b918748ababb09a80bfc2ef9f7377992f9
Yugansh-Varshney <101252107+Yugansh-Varshney@users.noreply.github.com>
2022-03-09 15:09:15 +0530

    Add files via upload

---
+++ product_of_2_no.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    
+    int num1, num2;
+    
+    cout<<"Enter your 1st number: \n";
+    cin>>num1;
+    
+    cout<<"Enter your 2nd number: \n";
+    cin>>num2;
+
+    cout<<"The product of 2 numbers is: "<< num1*num2;
+
+    return 0;
+
+}
